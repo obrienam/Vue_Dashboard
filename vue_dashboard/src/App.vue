ba4e637a622d3e12aa1a8482692710ba146999a4
Aidan O'Brien <obrienam@appstate.edu>
2019-11-22 20:07:30 -0500

    Transfered vue code from other repo

---
+++ vue_dashboard/src/App.vue
@@ -0,0 +1,163 @@
+<template>
+  <div id="app">
+    <b-navbar>
+      <template slot="brand">
+        <b-navbar-item>
+          <img src="../public/img/asu.png">
+        </b-navbar-item>
+      </template>
+    <template slot="start">
+      <b-navbar-item href="#">
+        Dashboard
+      </b-navbar-item>
+    </template>
+    </b-navbar>
+    <div id="page-wrap">
+    <b-tabs type="is-toggle" position="is-centered" mobile-burger="true" left="true" >
+      <b-tab-item label="Fiware">
+      <nav class="breadcrumb" aria-label="breadcrumbs">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li class="is-active"><a href="#" aria-current="page">Fiware</a></li>
+      </ul>
+      </nav>
+      <div class="tile is-ancestor">
+        <div class="tile is-parent">
+          <div class="tile is-child box">
+            <div class="tile is-child  box">
+              <p class="title">FiWare</p>
+              <img src="../public/img/svg/logo.svg" class="photon">
+            </div>
+            <div class="tile is-child  box">
+              <datatable/>
+            </div>
+          </div> 
+        </div>
+        
+        <div class="tile is-5 is-vertical is-parent">
+        <div class="tile is-child box">
+          <datatable/>
+        </div>
+        </div>
+        
+      </div>
+      
+      
+      </b-tab-item> 
+      <b-tab-item label="Thingsboard">
+      <nav class="breadcrumb" aria-label="breadcrumbs">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li class="is-active"><a href="#" aria-current="page">Thingsboard</a></li>
+      </ul>
+      </nav>
+      </b-tab-item>
+      <b-tab-item label="Table Experiments">
+        <nav class="breadcrumb" aria-label="breadcrumbs">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li class="is-active"><a href="#" aria-current="page">Table Experiments</a></li>
+        </ul>
+        </nav>
+        <h1>Test Data</h1>
+          <datatable/>
+      </b-tab-item>
+    </b-tabs>  
+    </div>
+
+  </div>
+  
+</template>
+
+<script>
+import 'buefy/dist/buefy.css'
+import Vue from 'vue'
+import Buefy from 'buefy'
+import 'font-awesome/css/font-awesome.min.css'
+import axios from 'axios'
+import datatable from './components/datatable.vue'
+
+Vue.use(Buefy, axios,datatable,{
+  defaultIconPack: "fa"
+})
+export default {
+  name: 'app',
+  components:{
+    datatable
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+/* Beginnings of a dark mode that follows the system setting.
+@media (prefers-color-scheme: dark) {
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: white;
+    background-color:black;
+    margin-top: 60px;
+  } 
+}*/
+
+img {
+  width: 100%;
+  height: 100%
+}
+.tabs.is-toggle li.is-active a{
+  background-color: rgb(255,210,4);
+  border-color:black;
+  color: black
+}
+
+.navbar-menu a{
+  color:black!important;
+}
+
+.pagination-list a{
+  background-color: rgb(255,210,4);
+}
+
+.pagination-list a.is-current{
+  background-color: rgb(255,210,4);
+  color:black;
+  border-color:black
+}
+.pagination-list a:active{
+  border-color:black
+}
+a.pagination-link.pagination-next:active{
+  border-color:black
+}
+a.pagination-link.pagination-previous:active{
+  border-color:black
+}
+.breadcrumb li a{
+  color:gray
+}
+.breadcrumb li.is-active a{
+  color:black
+}
+@keyframes rotate {
+  from {
+    transform: rotate(0deg);
+  }
+  to { 
+    transform: rotate(360deg);
+  }
+}
+img.photon {
+    animation: rotate 10.0s infinite linear;
+}
+
+</style>
---
+++ vue_dashboard/src/components/datatable.vue
@@ -0,0 +1,104 @@
+<template>
+    <div id="datatable">
+      <section v-if="err">
+        <p class="title">FiWare</p>
+        <p>No Data Detected!</p>
+      </section>
+      <section v-else>
+        <p class="title">Fiware Data</p>
+          
+          <b-table :data="d" :columns="columns" backend-sorting backend-pagination></b-table>
+        
+       
+      </section>
+    </div>
+</template>
+
+<script>
+import 'buefy/dist/buefy.css'
+import Vue from 'vue'
+import Buefy from 'buefy'
+import 'font-awesome/css/font-awesome.min.css'
+import axios from 'axios'
+import Component from "vue-class-component"
+
+Vue.use(Buefy, axios,{
+  defaultIconPack: "fa"
+})
+
+@Component 
+class datatable extends Vue{
+    
+  err=false;
+  d=[]; 
+  dusd=[];
+  dgb=[];
+  deur=[];
+       
+  //isPaginated= true; isPaginationSimple: false;paginationPosition: 'bottom', perPage: 5, currentPage: 1
+  columns=[
+    {
+      field:"code",
+      label:"Code",
+    },
+    {
+      field:"symbol",
+      label:"Symbol",
+    },
+    {
+      field:"rate",
+      label:"Rate",
+    },
+    {
+      field:"description",
+      label:"Description",
+    },
+    {
+      field:"rate_float",
+      label:"Rate_Float",
+    }
+  ];
+  mounted () {
+      axios
+      //.get('http://35.172.221.32:1026/v2/entities')
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => {
+   
+        this.dusd = response.data.bpi.USD;
+        this.dgb = response.data.bpi.GBP;
+        this.deur = response.data.bpi.EUR;
+        this.d.push(this.dusd)
+        this.d.push(this.dgb)
+        this.d.push(this.deur)
+      })
+  }
+
+} export default datatable;
+</script>
+<style scoped>
+#fiware_datatable {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.pagination-list a{
+  background-color: rgb(255,210,4);
+}
+
+.pagination-list a.is-current{
+  background-color: rgb(255,210,4);
+  color:black;
+  border-color:black
+}
+.pagination-list a:active{
+  border-color:black
+}
+a.pagination-link.pagination-next:active{
+  border-color:black
+}
+a.pagination-link.pagination-previous:active{
+  border-color:black
+}
+</style>
